e050e19043bbe73c640db5a6ef8205241e9adeb2
Dawid8plc <DSzelag123@gmail.com>
2024-07-13 10:52:21 +0200

    Fail silently in case we don't find the place to hook into

--- fkWaterFix/dllmain.cpp
+++ fkWaterFix/dllmain.cpp
@@ -35,13 +35,20 @@ BOOL APIENTRY DllMain(HMODULE hModule,
     {
     case DLL_PROCESS_ATTACH:
     {
+        try {
         std::filesystem::path a = std::filesystem::current_path();
         mainPath = a.generic_string();
 
         DWORD ColorTxtAfterLoadAddr = Hooks::scanPattern2("ColorTxtAfterLoad", "E9 38 FE FF FF 68 40 51 5B 00 B9 B8 4F 5B 00 E8 93 2E 00 00 6A 08 E8 B4 8B F7 FF 89 45 B4 C6 45 FC 04 83 7D B4 00 74 0D 8B 4D B4 E8 C4 8E F7 FF 89 45 94 EB 07 C7 45 94 00 00 00 00 8B 4D 94 89 4D B0 C6 45 FC 00 8B 55 A8 8B 45 B0");
         ColorTxtAfterLoadAddrRet = Hooks::scanPattern2("ColorTxtAfterLoadReturn", "8B 4D E4 83 C1 01 89 4D E4 8B 55 E4 3B 55 DC 0F 8D B3 01 00 00 8B 45 E4 50 8B 4D A8 E8 31 87 F7 FF 89 45 D4 8B 4D D4 51");
 
-        HookLib::InstallHook(reinterpret_cast<LPVOID>(ColorTxtAfterLoadAddr), &ColorTxtAfterLoadHook, HookLib::JMP_LONG);
+        if(ColorTxtAfterLoadAddr != NULL && ColorTxtAfterLoadAddrRet != NULL)
+            HookLib::InstallHook(reinterpret_cast<LPVOID>(ColorTxtAfterLoadAddr), &ColorTxtAfterLoadHook, HookLib::JMP_LONG);
+        }
+        catch(...)
+        {
+
+        }
     }
     break;
     case DLL_THREAD_ATTACH:
